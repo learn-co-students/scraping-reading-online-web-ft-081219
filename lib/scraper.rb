95ae3fba6e5a9ff22dd26cb9a2149b5ea66f6642
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-09 20:46:20 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
 
+
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+doc.css(".headline-26OIBN")
+
+puts doc.css(".headline-26OIBN").text.strip #how to grab the text from the class .headline-26OIBN .strip will clear up any extra blank spaces you may have. it will also take away the HTML elements 
+ 
+ 
 
 
---
+++ lib/scraper_two.rb
@@ -0,0 +1,11 @@
+require 'nokogiri'
+require 'open-uri'
+ 
+html = open("http://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+# courses = doc.css(".title-oE5vT4")
+# courses.each do |course|
+#   puts course.text.strip 
+# end
+p doc.css(".title-oE5vT4")[0].attributes
