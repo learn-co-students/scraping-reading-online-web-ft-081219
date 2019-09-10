cff7748666a910d7e80a7c4f81353040274125b2
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-10 21:08:34 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,7 +1,23 @@
 require 'nokogiri'
 require 'open-uri'
+ 
+doc = Nokogiri::HTML(open("http://flatironschool.com/"))
+puts doc.css(".site-header__hero__headline") 
 
-html = open("https://flatironschool.com/")
 
 
+# courses = doc.css(".tout__label.heading.heading--level-4")
+ 
+# courses.each do |course|      #Assigning doc.css to a variable and then use 
+#   puts course.text.strip      .each and puts plus text.strip to get the list 
+# end -->>                        below 
 
+# $1M in Scholarships for Women
+# What Kind of Coding Program is Right for You?
+# Attend an Online Info Session
+# Coding Bootcamp Prep
+# Online Software Engineering
+# Data Science Bootcamp Prep
+# Online Data Science
+
+# Not exactly the course listing as it scraped some other content as well - a great example how tricky scraping can be - but we've still achieved iteration!
