9586e668213e6fe410a0c92c6dc210a4e1215d9d
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-10 21:32:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,8 +4,8 @@ require 'open-uri'
 # html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://www.atlasobscura.com/things-to-do/brazil/"))
-# puts doc.css(".headline-26OIBN").text
- puts doc.css(".subtitle-sm.content-card-subtitle.js-subtitle-content")[0].text
+puts doc.css(".headline-26OIBN").text
+
 
 puts "hello"
 
