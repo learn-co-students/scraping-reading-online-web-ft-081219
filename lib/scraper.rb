14f5e2fa81686600cacd8d3f3ebd8a4dbbbb04fe
Emilia ☆ <emilia.mena26@gmail.com>
2019-09-10 21:35:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,5 +4,6 @@ require 'open-uri'
 # html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-puts doc.css(".headline-26OIBN").text
+#puts doc.css(".headline-26OIBN").text
 
+doc.css(".tout__label.heading.heading--level-4")
